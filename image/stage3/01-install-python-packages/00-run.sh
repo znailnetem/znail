d981be846dbed5eb1f9f72ac30877473ddb32e57
Andreas Nilsson <andni233@gmail.com>
2019-03-17 13:36:59 +0100

    Fixes to make image generation work.

--- image/stage3/01-install-python-packages/00-run.sh
+++ image/stage3/01-install-python-packages/00-run.sh
@@ -1,13 +1,15 @@
 #!/bin/bash -e
 
-install -d /opt/znail
+install -d "${ROOTFS_DIR}/opt/znail"
 install -v -m 600 /vagrant/requirements.txt "${ROOTFS_DIR}/opt/znail/requirements.txt"
 install -v -m 600 /vagrant/dist/pypi/*.whl "${ROOTFS_DIR}/opt/znail"
 
-ln -s /opt/znail/venv/lib/python3.5/site-packages/znail/netem/data/hub-ctrl "${ROOTFS_DIR}"/usr/local/bin/hub-ctrl
+rm -f "${ROOTFS_DIR}/usr/local/bin/hub-ctrl"
+ln -s /opt/znail/venv/lib/python3.5/site-packages/znail/netem/data/hub-ctrl "${ROOTFS_DIR}/usr/local/bin/hub-ctrl"
 
 on_chroot << EOF
 pushd /opt/znail
+python3 -m venv venv
 source venv/bin/activate
 pip3 install -r requirements.txt
 pip3 install *.whl
